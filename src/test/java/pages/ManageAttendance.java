a21a7e9b5ae802d786c9bb34f4972e3a76e9c965
ankurA987123 <ankur.kumar@apptadinc.com>
2024-08-30 16:10:26 +0530

    Updated the code.
    Merged the Shiv om code.

---
+++ src/test/java/pages/ManageAttendance.java
@@ -0,0 +1,60 @@
+
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import base.BaseTest;
+
+public class ManageAttendance extends BaseTest {
+	
+	WebElement btn_Workorder;
+	WebElement btn_More;
+	WebElement click_ViewJobSeekerSchedules;
+	WebElement btn_ViewTasks;
+	WebElement click_Openmenutoexplore;
+	WebElement click_Active;
+	WebElement actual_message;
+
+	
+	
+	
+	
+	public ManageAttendance(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public void clickOnbtn_Workorder() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_Workorder")))).click();
+	}
+	
+	public void clickOnbtn_More() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_More")))).click();
+	}
+	
+	public void clickOn_ViewJobSeekerSchedules() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_ViewJobSeekerSchedules")))).click();
+	}
+	
+	public void clickOnbtn_ViewTasks() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_ViewTasks")))).click();
+	}
+	
+	public void clickOn_Openmenutoexplore() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_Openmenutoexplore")))).click();
+	}
+	
+	public void clickOn_Active() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_Active")))).click();
+	}
+	
+	public String get_Actualmessage() {
+		   return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("actual_message")))).getText();
+	 }
+	
+}
---
+++ src/test/java/testcase/CreditStatementTest.java
@@ -0,0 +1,197 @@
+package testcase;
+
+import java.io.IOException;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.CreditStatement;
+import utilities.ReadXLData;
+
+@Listeners(base.Listeners.class)
+
+public class CreditStatementTest extends BaseTest {
+	CreditStatement CreditStatementTest;
+	
+	static String actualURL;
+	
+	
+	@Test(dataProviderClass = ReadXLData.class, dataProvider = "testData")
+	public void enterprise(String phoneOrEmail, String password, String amount, String Superadminemail, String Superadminpassword, String Verificationcode, String Expectedmessage) throws InterruptedException, IOException {
+		CreditStatementTest = new CreditStatement(driver);
+		
+		
+		CreditStatementTest.enterPhoneOrEmail(phoneOrEmail);
+		CreditStatementTest.enterPassword(password);
+		CreditStatementTest.clickOnBtn_Login();
+		CreditStatementTest.clickOnbtn_Credits();
+		CreditStatementTest.clickOnbtn_Topup();
+		CreditStatementTest.enteramount(amount);
+		CreditStatementTest.clickOnbtn_Paylater();
+		CreditStatementTest.clickOn_dropdown();
+		CreditStatementTest.clickOn_days();
+		CreditStatementTest.clickOn_checkbox();
+		CreditStatementTest.clickOnbtn_RequestCredits();
+		CreditStatementTest.clickOn_accounticon();
+		CreditStatementTest.clickOn_logout();
+		CreditStatementTest.enterSuperadminemail(Superadminemail);
+		CreditStatementTest.enterSuperadminpassword(Superadminpassword);
+		CreditStatementTest.clickOnBtn_Login();
+		CreditStatementTest.clickOnbtn_Credits();
+		CreditStatementTest.clickOn_approve();
+		CreditStatementTest.enterverificationcode(Verificationcode);
+		CreditStatementTest.clickOn_Next();
+		String actualMessage = CreditStatementTest.get_MessageForCreditstatus();
+		softAssert.assertEquals(actualMessage, Expectedmessage);
+		try {
+			softAssert.assertAll();
+		} catch (AssertionError e) {
+			assertionMessage.set(e.getMessage());
+			throw e;
+		}
+		
+	}
+	
+	@Test(dataProviderClass = ReadXLData.class, dataProvider = "testData")
+	public void enterprise1(String phoneOrEmail, String password, String amount, String Superadminemail, String Superadminpassword, String Verificationcode, String Expectedmessage) throws InterruptedException, IOException {
+		CreditStatementTest = new CreditStatement(driver);
+		
+		
+		CreditStatementTest.enterPhoneOrEmail(phoneOrEmail);
+		CreditStatementTest.enterPassword(password);
+		CreditStatementTest.clickOnBtn_Login();
+		CreditStatementTest.clickOnbtn_Credits();
+		CreditStatementTest.clickOnbtn_Topup1();
+		CreditStatementTest.enteramount(amount);
+		CreditStatementTest.clickOnbtn_Paylater();
+		CreditStatementTest.clickOn_dropdown();
+		CreditStatementTest.clickOn_days();
+		CreditStatementTest.clickOn_checkbox();
+		CreditStatementTest.clickOnbtn_RequestCredits();
+		CreditStatementTest.clickOn_accounticon();
+		CreditStatementTest.clickOn_logout();
+		CreditStatementTest.enterSuperadminemail(Superadminemail);
+		CreditStatementTest.enterSuperadminpassword(Superadminpassword);
+		CreditStatementTest.clickOnBtn_Login();
+		CreditStatementTest.clickOnbtn_Credits();
+		CreditStatementTest.clickOn_approve();
+		CreditStatementTest.enterverificationcode(Verificationcode);
+		CreditStatementTest.clickOn_Next();
+		String actualMessage = CreditStatementTest.get_MessageForCreditstatus();
+		softAssert.assertEquals(actualMessage, Expectedmessage);
+		try {
+			softAssert.assertAll();
+		} catch (AssertionError e) {
+			assertionMessage.set(e.getMessage());
+			throw e;
+		}
+		
+	}
+	
+	@Test(dataProviderClass = ReadXLData.class, dataProvider = "testData")
+	public void enterprise2(String phoneOrEmail, String password, String expectedURL) throws InterruptedException, IOException {
+		CreditStatementTest = new CreditStatement(driver);
+		
+		
+		CreditStatementTest.enterPhoneOrEmail(phoneOrEmail);
+		CreditStatementTest.enterPassword(password);
+		CreditStatementTest.clickOnBtn_Login();
+		CreditStatementTest.clickOnbtn_Credits();
+		CreditStatementTest.clickOn_ViewStatement();
+		actualURL = driver.getCurrentUrl();
+		softAssert.assertEquals(actualURL, expectedURL);
+		try {
+			softAssert.assertAll();
+		} catch (AssertionError e) {
+			assertionMessage.set(e.getMessage());
+			throw e;
+		}
+	}
+	
+	@Test(dataProviderClass = ReadXLData.class, dataProvider = "testData")
+	public void enterprise3(String phoneOrEmail, String password) throws InterruptedException, IOException {
+		CreditStatementTest = new CreditStatement(driver);
+		
+		
+		CreditStatementTest.enterPhoneOrEmail(phoneOrEmail);
+		CreditStatementTest.enterPassword(password);
+		CreditStatementTest.clickOnBtn_Login();
+		CreditStatementTest.clickOnbtn_Credits();
+		CreditStatementTest.clickOn_ExporttoXLS();
+	}
+	
+	@Test(dataProviderClass = ReadXLData.class, dataProvider = "testData")
+	public void enterprise4(String phoneOrEmail, String password) throws InterruptedException, IOException {
+		CreditStatementTest = new CreditStatement(driver);
+		
+		
+		CreditStatementTest.enterPhoneOrEmail(phoneOrEmail);
+		CreditStatementTest.enterPassword(password);
+		CreditStatementTest.clickOnBtn_Login();
+		CreditStatementTest.clickOnbtn_Credits();
+		CreditStatementTest.clickOn_ViewStatement();
+		CreditStatementTest.clickOn_ExporttoXLS();
+	}
+	
+	@Test(dataProviderClass = ReadXLData.class, dataProvider = "testData")
+	public void enterprise5(String phoneOrEmail, String password, String Mobilenumber, String UPIID, String expectedmessage) throws InterruptedException, IOException {
+		CreditStatementTest = new CreditStatement(driver);
+		
+		
+		CreditStatementTest.enterPhoneOrEmail(phoneOrEmail);
+		CreditStatementTest.enterPassword(password);
+		CreditStatementTest.clickOnBtn_Login();
+		CreditStatementTest.clickOnbtn_Credits();
+		CreditStatementTest.clickOnbtn_Pay();
+		WebElement frame = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[@class='razorpay-checkout-frame']")));
+		driver.switchTo().frame(frame);
+		CreditStatementTest.enterMobilenumber(Mobilenumber);
+		CreditStatementTest.clickOnbtn_Continue();
+		CreditStatementTest.clickOn_upi();
+		CreditStatementTest.enterUPIID(UPIID);
+		CreditStatementTest.clickOnbtn_verifyandpay();
+		String actualMessage = CreditStatementTest.get_MessageForPaymentSuccessful();
+		softAssert.assertEquals(actualMessage, expectedmessage);
+		try {
+			softAssert.assertAll();
+		} catch (AssertionError e) {
+			assertionMessage.set(e.getMessage());
+			throw e;
+		}
+	}
+	
+	@Test(dataProviderClass = ReadXLData.class, dataProvider = "testData")
+	public void enterprise6(String phoneOrEmail, String password, String Mobilenumber, String UPIID, String expectedmessage) throws InterruptedException, IOException {
+		CreditStatementTest = new CreditStatement(driver);
+		
+		
+		CreditStatementTest.enterPhoneOrEmail(phoneOrEmail);
+		CreditStatementTest.enterPassword(password);
+		CreditStatementTest.clickOnBtn_Login();
+		CreditStatementTest.clickOnbtn_Credits();
+		CreditStatementTest.clickOnbtn_Pay1();
+		WebElement frame = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[@class='razorpay-checkout-frame']")));
+		driver.switchTo().frame(frame);
+		CreditStatementTest.enterMobilenumber(Mobilenumber);
+		CreditStatementTest.clickOnbtn_Continue();
+		CreditStatementTest.clickOn_upi();
+		CreditStatementTest.enterUPIID(UPIID);
+		CreditStatementTest.clickOnbtn_verifyandpay();
+		String actualMessage = CreditStatementTest.get_MessageForPaymentSuccessful();
+		softAssert.assertEquals(actualMessage, expectedmessage);
+		try {
+			softAssert.assertAll();
+		} catch (AssertionError e) {
+			assertionMessage.set(e.getMessage());
+			throw e;
+		}
+	}
+	
+}
+ 
