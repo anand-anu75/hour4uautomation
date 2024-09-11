e1ab6f7b6273ddef47a91b156b0d9f3da710d840
Shivom28 <shivom.kumari@apptadinc.com>
2024-09-11 09:46:08 +0530

    initial commit

---
+++ src/test/java/testcase/Assignvendortest.java
@@ -0,0 +1,58 @@
+package testcase;
+
+import java.io.IOException;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.Assignvendor;
+import utilities.ReadXLData;
+
+public class Assignvendortest extends BaseTest {
+	
+	Assignvendor Assignvendortest;
+	
+	
+	@Test(dataProviderClass = ReadXLData.class, dataProvider = "testData", description = "Verify Assign workorder to Vendors")
+	public void Assign_workorder_to_vendor(String phoneOrEmail, String password, String Date, String Workordertitle, String expectedmessage) throws InterruptedException, IOException {
+		Assignvendortest = new Assignvendor(driver);
+		
+		
+		Assignvendortest.enterPhoneOrEmail(phoneOrEmail);
+		Assignvendortest.enterPassword(password);
+		Assignvendortest.clickOnBtn_Login();
+		Assignvendortest.clickOnbtn_workorder();
+		Assignvendortest.clickOnbtn_createworkorder();
+		Assignvendortest.clickOn_selectprojects();
+		Assignvendortest.clickOnbtn_Adddetails();
+		Assignvendortest.clickOn_location();
+		Assignvendortest.enterDate(Date);
+		Assignvendortest.clickOn_Starttime();
+		Assignvendortest.clickOnSelect_Starttime();
+		Assignvendortest.clickOn_Endtime();
+		Assignvendortest.clickOnSelect_Endtime();
+		 Actions actions = new Actions(driver); 
+		 actions.sendKeys(Keys.ESCAPE).perform();
+		 Thread.sleep(2000);
+		Assignvendortest.clickOnbtn_Addbilling();
+		Assignvendortest.clickOnbtn_Addtitle();
+		Assignvendortest.enterWorkordertitle(Workordertitle);
+		Assignvendortest.clickOn_DropDown();
+		Assignvendortest.clickOn_AssignVendor();
+		Assignvendortest.clickOnbtn_SeeSummary();
+		Assignvendortest.clickOnbtn_SubmitForApproval();
+		String actualMessage = Assignvendortest.get_MessageForWorkorderAddedSuccessfully();
+		softAssert.assertEquals(actualMessage, expectedmessage);
+		try {
+			softAssert.assertAll();
+		} catch (AssertionError e) {
+			assertionMessage.set(e.getMessage());
+			throw e;
+		}
+		
+	}
+	
+}
+    
