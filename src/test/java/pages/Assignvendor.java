7d2a1ae0bdbf2a85d46621c0aa9791e407e7b1eb
ankurA987123 <ankur.kumar@apptadinc.com>
2024-09-19 15:39:06 +0530

    Updated code
    Updated code

--- src/test/java/pages/Assignvendor.java
+++ src/test/java/pages/Assignvendor.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import base.BaseTest;
 
 public class Assignvendor extends BaseTest {
-	
+
 	WebElement txt_phoneOrEmail;
 	WebElement txt_password;
 	WebElement btn_login;
@@ -31,117 +31,111 @@ public class Assignvendor extends BaseTest {
 	WebElement click_dropdown;
 	WebElement Assign_vendor;
 	WebElement message_workorderaddedsuccessfully;
-	
+
 	public Assignvendor(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 
 	}
-	
+
 	public void enterPhoneOrEmail(String phoneOrEmail) {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("txt_phoneOrEmail"))))
 				.sendKeys(phoneOrEmail);
 	}
-	
+
 	public void enterPassword(String password) {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("txt_password"))))
 				.sendKeys(password);
 	}
-	
+
 	public void clickOnBtn_Login() {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(loc.getProperty("btn_login")))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(loc.getProperty("btn_login")))).click();
 
 	}
-	
+
 	public void clickOnbtn_workorder() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_workorder"))))
-		.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_workorder")))).click();
 	}
-	
+
 	public void clickOnbtn_createworkorder() {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_createworkorder"))))
-		.click();
+				.click();
 	}
-	
+
 	public void clickOn_selectprojects() {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_selectprojects"))))
-		.click();
+				.click();
 	}
-	
+
 	public void clickOnbtn_Adddetails() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_Adddetails"))))
-		.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_Adddetails")))).click();
 	}
-	
+
 	public void clickOn_location() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_location"))))
-		.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_location")))).click();
 	}
-	
+
 	public void enterDate(String Date) {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("txt_date"))))
-				.sendKeys(Date);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("txt_date")))).sendKeys(Date);
 	}
-	
+
 	public void clickOn_Starttime() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_Starttime"))))
-		.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_Starttime")))).click();
 	}
-	
+
 	public void clickOnSelect_Starttime() {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("select_Starttime"))))
-		.click();
+				.click();
 	}
-	
+
 	public void clickOn_Endtime() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_Endtime"))))
-		.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_Endtime")))).click();
 	}
-	
+
 	public void clickOnSelect_Endtime() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("select_Endtime"))))
-		.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("select_Endtime")))).click();
 	}
-	
+
 	public void clickOnbtn_Addbilling() {
-		WebElement btn_Addbilling = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_Addbilling"))));
-		((JavascriptExecutor)driver).executeScript("arguments[0].click();", btn_Addbilling);
+		WebElement btn_Addbilling = wait
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_Addbilling"))));
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", btn_Addbilling);
 	}
-	
+
 	public void clickOnbtn_Addtitle() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_Addtitle"))))
-		.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_Addtitle")))).click();
 	}
-	
+
 	public void enterWorkordertitle(String Workordertitle) {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("txt_Workordertitle"))))
 				.sendKeys(Workordertitle);
 	}
-	
+
 	public void clickOnbtn_SeeSummary() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_SeeSummary"))))
-		.click();
+		WebElement SeeSummary_btn = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath(loc.getProperty("btn_SeeSummary"))));
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+		jsExecutor.executeScript("arguments[0].click();", SeeSummary_btn);
 	}
-	
+
 	public void clickOnbtn_SubmitForApproval() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_approval"))))
-		.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("btn_approval")))).click();
 	}
-	
+
 	public void clickOn_DropDown() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_dropdown"))))
-		.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("click_dropdown")))).click();
 	}
-	
+
 	public void clickOn_AssignVendor() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("Assign_vendor"))))
-		.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("Assign_vendor")))).click();
 	}
-	
+
 	public String get_MessageForWorkorderAddedSuccessfully() {
-	   	return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loc.getProperty("message_workorderaddedsuccessfully")))).getText();
-	    	
- }
-	
+		return wait
+				.until(ExpectedConditions
+						.visibilityOfElementLocated(By.xpath(loc.getProperty("message_workorderaddedsuccessfully"))))
+				.getText();
+
+	}
 
 }
